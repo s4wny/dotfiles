5b6d6a6dc81f9a65c5256ac4bccf28b4ffa87f58
Sawny <s4wny@users.noreply.github.com>
2018-03-03 20:35:07 +0100

    Update template.cpp
    Faster integer input

--- programming-competitions-env/template.cpp
+++ programming-competitions-env/template.cpp
@@ -10,6 +10,21 @@ typedef long long ll;
 typedef pair<int, int> ii;
 typedef vector<int> vi;
 
+// Faster integer input
+// Taken from https://stackoverflow.com/q/9052757/996028
+#define getcx getchar_unlocked
+inline void inp( int &n ) 
+{
+    n=0;
+    int ch=getcx();
+    int sign=1;
+    while(ch < '0' || ch > '9') { if(ch=='-') sign=-1; ch=getcx(); }
+
+    while(ch >= '0' && ch <= '9')
+        n = (n<<3)+(n<<1) + ch-'0', ch=getcx();
+    n=n*sign;
+}
+
 int main() {
     cin.sync_with_stdio(0);
     
